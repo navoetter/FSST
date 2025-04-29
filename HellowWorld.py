9f9bff42ceb2bcd4e15058b1a87be902c4509fdf
navoetter <navoetter@tsn.at>
2025-04-29 13:10:43 +0200

    Rechner

--- HellowWorld.py
+++ HellowWorld.py
@@ -1,2 +1,9 @@
 # HelloWorld.py in Github
 print("Hallo 4CHEL")
+
+zahl1 = float(input("Gib die erste Zahl ein: "))
+zahl2 = float(input("Gib die zweite Zahl ein: "))
+
+summe = zahl1 + zahl2
+
+print("Die Summe ist:", summe)
